15ce4504740bd1ee92f4e800a57233e889a197ec
pranav s <dev.pranabsk@gmail.com>
2022-09-07 08:43:39 +0530

    Built-in context api set dynamic data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
+
+export const CategoryContext = createContext();
 
 function App() {
   //local state
@@ -10,10 +12,14 @@ function App() {
 
   return (
     <div className="App">
+
+      <CategoryContext.Provider value="laptop"> {/* value={count} -or dynamic value */}
       <span>Count Value State: {count}</span>
       <Header count={count} setCount={setCount}/>
       <Home count={count} setCount={setCount}/>
       <Contact count={count}/>
+      </CategoryContext.Provider>
+     
     </div>
   );
 }
--- src/components2/CategoriesDetail/CategoriesDetail.js
+++ src/components2/CategoriesDetail/CategoriesDetail.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CategoryContext } from '../../App';
 
 const CategoriesDetail = props => {
+    const category =  useContext(CategoryContext)
     const count = props.count;
     return (
         <div>
             CategoriesDetail: {count}
+            <h6>selected category: {category }</h6>
         </div>
     );
 };
